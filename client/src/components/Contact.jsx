7a440f8afbe2c189e2a74a8af6c450f0ffdd438c
dennis <kipkiruidennis25@gmail.com>
2025-11-23 12:28:18 +0300

    contact API endpoint

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -18,7 +18,7 @@ export default function Contact() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5000/api/contact", {
+      const res = await fetch(`${API_URL}api/contact`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
